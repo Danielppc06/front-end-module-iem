39d88aba0606bfc0e7e13d195f093e24a19cc3b2
Danielppc06 <danielppc06@gmail.com>
2025-04-09 21:00:17 +0200

    Added class one

---
+++ index.js
@@ -0,0 +1,10 @@
+let counter = 0;
+
+console.log;
+
+const consSpan = document.getElementById("counter-button");
+countSpan.innerText = counter;
+
+console.log(button);
+
+button.addEventListener("click", () => {});
