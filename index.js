27e1a2223b35517ae46f6b4717f281db36a9e5c0
Danielppc06 <danielppc06@gmail.com>
2025-05-05 17:41:08 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-let counter = 0;
-
-console.log;
-
-const consSpan = document.getElementById("counter-button");
-countSpan.innerText = counter;
-
-console.log(button);
-
-button.addEventListener("click", () => {});
+console.log("Hello, world!");
---
+++ typescript-intro.ts
@@ -0,0 +1,6 @@
+console.log("Hola Typescript");
+let age: number = 23;
+let nickName: string = "Jhon";
+let isStudent: boolean = true;
+
+console.log(age, nickName, isStudent);
